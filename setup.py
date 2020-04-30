cb239c8b42bea03cf0e39a93e20651fc7d60abac
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2020-04-30 08:14:57 -0400

    Added more metadata values for setuptools

--- setup.py
+++ setup.py
@@ -8,16 +8,16 @@ setup(name='collective.geo.geopoint',
       description="z3c.form geopoint field support",
       long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from https://pypi.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='',
+      keywords='plone geo maps z3c form geopoint field',
       author='Giorgio Borelli',
       author_email='giorgio@giorgioborelli.it',
-      url='https://svn.plone.org/svn/collective/collective.geo.geopoint',
+      url='https://github.com/collective/collective.geo.geopoint',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective', 'collective.geo'],
